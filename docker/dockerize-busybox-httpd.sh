b5b92d8965652ce1e60808fccc4c66959300e1e3
David Leonhart <leozilla@gmail.com>
2017-10-27 15:40:24 +0200

    Edit makefile

--- docker/dockerize-busybox-httpd.sh
+++ docker/dockerize-busybox-httpd.sh
@@ -5,5 +5,6 @@ dockerize -t leozilla/indoorcup-web \
    --add-file ../index.html /www/ \
    --add-file ../img/logo.jpg /www/img \
    --entry '/bin/busybox' \
-   --cmd 'httpd -f -p 80 -h /www && mkdir -p /www/img' \
+   --cmd 'httpd -f -p 80 -h /www' \
+   --cmd 'mkdir -p /www/img' \
    /bin/busybox
