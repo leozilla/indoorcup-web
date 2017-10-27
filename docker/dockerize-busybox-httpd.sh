82f3e96897919a8ec83d8b9b173aa4a15e22a198
David Leonhart <leozilla@gmail.com>
2017-10-27 15:36:30 +0200

    Try other cmd

--- docker/dockerize-busybox-httpd.sh
+++ docker/dockerize-busybox-httpd.sh
@@ -2,9 +2,9 @@
 set -e
 
 dockerize -t leozilla/indoorcup-web \
-   --cmd 'mkdir -p /www/img' \
    --add-file ../index.html /www/ \
    --add-file ../img/logo.jpg /www/img \
    --entry '/bin/busybox' \
+   --cmd 'mkdir -p /www/img' \
    --cmd 'httpd -f -p 80 -h /www' \
    /bin/busybox
