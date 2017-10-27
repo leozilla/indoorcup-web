05aeda48d39600d44f8cef05cfc358e7a6b4073c
David Leonhart <leozilla@gmail.com>
2017-10-27 15:37:16 +0200

    Concat cmd

--- docker/dockerize-busybox-httpd.sh
+++ docker/dockerize-busybox-httpd.sh
@@ -5,6 +5,5 @@ dockerize -t leozilla/indoorcup-web \
    --add-file ../index.html /www/ \
    --add-file ../img/logo.jpg /www/img \
    --entry '/bin/busybox' \
-   --cmd 'mkdir -p /www/img' \
-   --cmd 'httpd -f -p 80 -h /www' \
+   --cmd 'httpd -f -p 80 -h /www && mkdir -p /www/img' \
    /bin/busybox
