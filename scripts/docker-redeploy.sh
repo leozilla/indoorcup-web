81f0364f92b6aaa396d52aa51fff90a1580baa63
David Leonhart <leozilla@gmail.com>
2017-10-27 20:18:27 +0200

    Updated redeploy script

--- scripts/docker-redeploy.sh
+++ scripts/docker-redeploy.sh
@@ -4,11 +4,11 @@ set -e
 cd ..
 make build test push
 
-OLD_CONTAINER_ID=`docker ps | grep leozilla/indoorcup-web | awk '{print $1}'`
+OLD_CONTAINER_ID=`docker ps | grep indoorcup-web | awk '{print $1}'`
 if [ ! -z "$OLD_CONTAINER_ID" ];
 then
     docker kill $OLD_CONTAINER_ID
 fi
 
-docker run -d -p 80:80 leozilla/indoorcup-web
+docker run -d -p 80:80 leozilla/indoorcup-web --name indoorcup-web
 
