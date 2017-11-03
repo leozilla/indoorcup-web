5b542cedad08da238d17ce3d38d7ae5e42b06510
David Leonhartsberger <david.le@mobfox.com>
2017-11-03 12:14:31 +0100

    Stop container then remove it

--- scripts/docker-redeploy.sh
+++ scripts/docker-redeploy.sh
@@ -1,20 +1,30 @@
 #!/usr/bin/env bash
 set -e
 
-OLD_CONTAINER_ID=`docker ps | grep indoorcup-web | awk '{print $1}'`
-if [ -z "$OLD_CONTAINER_ID" ];
+OLD_RUNNING_CONTAINER_ID=`docker ps | grep indoorcup-web | awk '{print $1}'`
+if [ -z "$OLD_RUNNING_CONTAINER_ID" ];
 then
-    OLD_CONTAINER_ID=`docker ps | grep 0.0.0.0:80 | awk '{print $1}'`
+    OLD_RUNNING_CONTAINER_ID=`docker ps | grep 0.0.0.0:80 | awk '{print $1}'`
 fi
 
+if [ ! -z "$OLD_RUNNING_CONTAINER_ID" ];
+then
+    echo "Stop container $OLD_RUNNING_CONTAINER_ID"
+    docker stop OLD_RUNNING_CONTAINER_ID
+else
+    echo "No container to stop"
+fi
+
+
+OLD_CONTAINER_ID=`docker ps -a | grep indoorcup-web | awk '{print $1}'`
 if [ ! -z "$OLD_CONTAINER_ID" ];
 then
-    echo "Killing container $OLD_CONTAINER_ID"
-    docker kill $OLD_CONTAINER_ID
-    docker rm $OLD_CONTAINER_ID
+    echo "Removing container $OLD_CONTAINER_ID"
+    docker rm -f $OLD_CONTAINER_ID
 else
-    echo "No container to kill"
+    echo "No container to remove"
 fi
+
 
 echo "Starting new container"
 docker run -d -p 80:80 --name indoorcup-web leozilla/indoorcup-web
